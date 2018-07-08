c2261151e622ba0cb28fe02d536b56fbb247e186
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-08 14:52:15 -0500

    added export to index file for Pillar Section component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Wrapper from '../../components/Wrapper';
 import Jumbotron from '../../components/Jumbotron';
 import PillarSection from '../../components/Pillar';
+
 class Home extends React.Component {
     render () {
         return (
