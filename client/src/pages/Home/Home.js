199a8e25cbbfd8fbdd8faab8382f55c3beed90c5
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-08 14:48:46 -0500

    made home page a react component with render return

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Wrapper from '../../components/Wrapper';
 import Jumbotron from '../../components/Jumbotron';
 import PillarSection from '../../components/Pillar';
-
-const Home = () => {
-    < Wrapper >
-        < Jumbotron />
-        < PillarSection />
-        < Mission />
-    </ Wrapper > 
+class Home extends React.Component {
+    render () {
+        return (
+            < Wrapper >
+                < Jumbotron />
+                < PillarSection />
+                < Mission />
+            </ Wrapper > 
+        )
+    }
 };
 
 export default Home;
