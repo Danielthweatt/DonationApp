04f1e124f3f9586297d76cdc8f0dc6d19e2a5908
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-08 14:44:47 -0500

    took scaffold styling out of home page and put into pillar section component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import Wrapper from '../../components/Wrapper';
 import Jumbotron from '../../components/Jumbotron';
-import Pillar from '../../components/Pillar';
+import PillarSection from '../../components/Pillar';
 
 const Home = () => {
     < Wrapper >
         < Jumbotron />
-        <div className="row">
-            <div className="col-md-4">
-                < Pillar />
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-md-4">
-                < Pillar />
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-md-4">
-                < Pillar />
-            </div>
-        </div>
+        < PillarSection />
         < Mission />
     </ Wrapper > 
 };
