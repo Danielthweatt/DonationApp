afe91d14589c020d497eb9cbec2e6cab4d88cc49
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-10 19:58:16 -0500

    setup route for donations page and started working on home page components

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Wrapper from '../../components/Wrapper';
 import Jumbotron from '../../components/Jumbotron';
+import Header from '../../components/Header';
 import PillarSection from '../../components/Pillar';
+import Mission from '../../components/Mission';
 
 class Home extends React.Component {
     render () {
         return (
             < Wrapper >
+                < Header />
                 < Jumbotron />
-                < PillarSection />
-                < Mission />
+                {/* < PillarSection /> */}
+                {/* < Mission /> */}
             </ Wrapper > 
         )
     }
