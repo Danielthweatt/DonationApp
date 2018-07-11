6e769981b1d903636ba18c1f3ddf76937a64270a
Danielthweatt <dthweatt192@yahoo.com>
2018-07-11 17:05:53 -0500

    configured and began integrating passport.js authentication

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 //Dependecies
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+const User = require('../models').User;
 const routes = require('./routes');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Serving Static Assets
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
+app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport/passport.js')(passport, User);
 
 // Routes
 // app.use(routes);
 
 // Connection to MongoDB
+// mongoose.Promise = Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donation_app');
 
 // API server Start
