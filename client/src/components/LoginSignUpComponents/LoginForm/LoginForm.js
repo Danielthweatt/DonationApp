937b277d59304dc22958969f85548abc07ba92a9
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-12 19:51:22 -0500

    created login page first draft

---
+++ client/src/components/LoginSignUpComponents/LoginForm/LoginForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Input from '../../Input'; 
+
+class LoginForm extends Component {
+
+	state= {
+		email:"",
+		password:""
+	}
+
+	handleEmailInput = e => {
+		this.setState({email: e.target.value})
+	}
+
+	handlePasswordInput = e => {
+		//console.log(e.target.value)
+		this.setState({name: e.target.value})
+	}
+
+	render() {
+		return (
+			<form action="/signin" method="post">
+				<div>
+					<label>Username:</label>
+					<Input title = "Name" name="email" handleInput={this.handleEmailInput}/>
+				</div>
+				<div>
+					<label>Password:</label>
+					<Input title = "Name" type="password" name="password" handleInput={this.handlePasswordInput}/>
+				</div>
+				<div>
+					<input type="submit"/>
+				</div>
+			</form>
+		)
+	}
+};
+
+export default LoginForm;
