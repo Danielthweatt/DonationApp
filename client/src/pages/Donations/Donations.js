28755b9fa938c6308f3389526568c5cc8d2dbe2b
m-fiks <fiksd005@umn.edu>
2018-07-12 20:14:53 -0500

    get button value

--- client/src/pages/Donations/Donations.js
+++ client/src/pages/Donations/Donations.js
@@ -8,11 +8,40 @@ import Footer from '../../components/Footer/Footer';
 
 class Donations extends Component {
 
+	state = {
+		five:"",
+		ten:"",
+		twenty:""
+	}
+
+	handleFive(e){
+		//console.log(e.target.value)
+		this.setState({five: e.target.value})
+	}
+
+	handleTen(e){
+		console.log(e.target.value)
+		this.setState({ten: e.target.value})
+	}
+
+	handleTwenty(e){
+		console.log(e.target.value)
+		this.setState({twenty: e.target.value})
+	}
+
+	handleCustom(e){
+	}
+
 	render() {
 		return (
 			<Wrapper>
 				<Header />
-				<DonateOptions />
+				<DonateOptions 
+					numValue="8.00"
+					handleFive={this.handleFive}
+					handleTen={this.handleTen}
+					handleTwenty={this.handleTwenty}
+				/>
 				<DonationInput/>
 				<DonationImpact />
 				<Footer />
