ec7c9e72f82456d444714c641112da2b7a7d162c
Danielthweatt <dthweatt192@yahoo.com>
2018-07-13 13:47:04 -0500

    worked on server, model, routes, passport.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const stripe = require("stripe")("sk_test_TwTTlid3GeOG6YPydOjARw4I");
 
 module.exports = function(app, passport, User){
 
-	// Charge Route
+	//Charge Route
 	app.post("/charge", async(req,res) => {
 		try {
 			let {status} = await stripe.charges.create({
@@ -22,29 +22,74 @@ module.exports = function(app, passport, User){
 	});
 
 	//Sign-Up Route
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/',
-		failureRedirect: '/signup',
-		failureFlash: true
-	}));
-
-	//Sign-In Route
-	app.post('/signin', passport.authenticate('local-signin', {
-		successRedirect: '/',
-		failureRedirect: '/signin',
-		failureFlash: true 
-	}));
-
-	//Sign-Out Route
-	app.post('/logout', function(req, res){
-		req.session.destroy(function(err){
-			res.redirect('/');
+	app.post('/signup', (req, res) => {
+		console.log('User signup route hit');
+		const { email, password } = req.body;
+		User.findOne({ email: email }, (err, user) => {
+			if (err) {
+				console.log('User signup db search error: ', err);
+				res.json(err);
+			} else if (user) {
+				res.json({
+					error: `Sorry, there is already a user with the username: ${email}`
+				});
+			}
+			else {
+				const newUser = new User({
+					email: email,
+					password: password
+				});
+				newUser.save((err, savedUser) => {
+					if (err) return res.json(err);
+					res.json(savedUser);
+				});
+			}
 		});
 	});
 
-	//React App
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	//Sign-In Route
+	app.post(
+		'/signin',
+		function (req, res, next) {
+			console.log('User signin route hit, req.body: ');
+			console.log(req.body);
+			next();
+		},
+		passport.authenticate('local'),
+		(req, res) => {
+			console.log('User logged in: ' + req.user);
+			var userInfo = {
+				email: req.user.email
+			};
+			res.send(userInfo);
+		}
+	);
+
+	//Check to see if signed-in Route
+	app.get('/signin-check', (req, res, next) => {
+		console.log('Signin-check route hit, req.user: ');
+		console.log(req.user);
+		if (req.user) {
+			res.json({ user: req.user });
+		} else {
+			res.json({ user: null });
+		}
 	});
+
+	//Sign-Out Route
+	app.post('/signout', (req, res) => {
+		if (req.user) {
+			console.log('Signing out.');
+			req.logout();
+			res.send({ msg: 'logging out' });
+		} else {
+			res.send({ msg: 'no user to log out' });
+		}
+	});
+
+	//React App
+	// app.get('*', function(req, res) {
+	// 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	// });
 
 };
--- server.js
+++ server.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const flash = require('connect-flash');
 const passport = require('passport');
 const User = require('./models').User;
 const mongoose = require('mongoose');
-const routes = require('./routes');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,12 +12,23 @@ const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
-app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized:true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(flash());
+app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized:false}));
+app.use( (req, res, next) => {
+	console.log('req.session', req.session);
+	return next();
+});
 app.use(passport.initialize());
+app.use( (req, res, next) => {
+	console.log('req.session', req.session);
+	return next();
+});
 app.use(passport.session());
+app.use( (req, res, next) => {
+	console.log('req.user', req.user);
+	return next();
+});
 
 // Configure Passport
 require('./config/passport/passport.js')(passport, User);
@@ -33,8 +41,8 @@ require('./routes')(app, passport, User);
 
 
 // Connection to MongoDB
-// mongoose.Promise = Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donation_app');
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donation_app');
 
 // API server Start
 app.listen(PORT, function() {
