8b8dad9260af36e78d3f9a6653efeaaa2d65ba96
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-12 20:04:17 -0500

    fixed package json bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import About from './pages/About/About';
 import Home from './pages/Home';
+import Login from './pages/Login'; 
 import Donations from './pages/Donations';
 import Header from './components/Header/Header'; 
 import Wrapper from './components/Wrapper/Wrapper'; 
-import Login from './components/LoginSignUpComponents/LoginForm'; 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
