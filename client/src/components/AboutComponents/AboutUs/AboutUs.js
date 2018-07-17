34b5d540373eb9d620426470e78f5d1359bca7b8
Cavan Wagg <waggx002@gmail.com>
2018-07-17 18:57:32 -0500

    removed offensive comment

--- client/src/components/AboutComponents/AboutUs/AboutUs.js
+++ client/src/components/AboutComponents/AboutUs/AboutUs.js
@@ -9,7 +9,7 @@ const AboutUs = () => (
 			<div class="col">
 				<div class="card-block px-2">
 					<h4 class="card-title">Something about Love</h4>
-					<p class="card-text">Infuse your life with action. Don't wait for it to happen. Make it happen. Make your own future. Make your own hope. Make your own love. And whatever your beliefs, honor your creator, not by passively waiting for grace to come down from upon high, but by doing what you can to make grace happen... yourself, right now, right down here on Earth.</p>
+					<p class="card-text">Infuse your life with action. Don't wait for it to happen. Make it happen. Make your own future.</p>
 				</div>
 			</div>
 		</div>
