bfc1ff70fd8ec4a2419306f8c5581228ebc274a0
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-19 20:06:36 -0500

    started styling the home page components

--- client/src/components/HomeComponents/Jumbotron/Jumbotron.js
+++ client/src/components/HomeComponents/Jumbotron/Jumbotron.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import Button from '@material/react-button/dist'; // /index.js is implied
+// import Button from '@material/react-button/dist'; // /index.js is implied
+import './Jumbotron.css'
 
 class Jumbotron extends React.Component {
     render () {
         return (
-            <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-4"></hr>
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <div className="jumbotron jumbotron-fluid">
+                {/* This empty div for color gradient overlay on top of image */}
+                <div className="color-overlay"></div>
+                <div className="inner">
+                    <center>
+                        <h1 className="display-4">Love Foundation</h1>
+                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    </center>
+                </div>
+                {/* <hr className="my-4"></hr>
                 <p className="lead">
                     <Button raised className='button-alternate' onClick={() => console.log('clicked!')}>
                         Learn more
                     </Button>
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
+                </p> */}
             </div>
         )
     }
