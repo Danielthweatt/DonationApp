51d2e1e21909ce26e2bcf46c97d73f4b1f42b879
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-07-21 13:27:44 -0500

    finished some styling components for home page

--- client/src/components/HomeComponents/Mission/Mission.js
+++ client/src/components/HomeComponents/Mission/Mission.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import './Mission.css'
+import ButtonPrimary from '../../Buttons/ButtonPrimary'
 
 class Mission extends React.Component {
 	render () {
 		return (
-			<div className="card text-center">
+			<div className="card mission-card text-center">
 				<center>
                     <h2>Our Mission</h2>
                     <hr></hr>
                 </center>
-                <br></br>
-				{/* <div className="card-header">
-                    MISSION!
-				</div> */}
 				<div className="card-body">
-					<h5 className="card-title">Special title treatment</h5>
-					<p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-					<a href="#" className="btn btn-primary">Go somewhere</a>
-				</div>
-				<div className="card-footer text-muted">
-                    2 days ago
+					<p className="card-text">Making sure the Earth exists for the next generation.</p>
+					<ButtonPrimary linkUrl="/about">About Us</ButtonPrimary>
 				</div>
 			</div>
 		);
