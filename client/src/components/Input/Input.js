e0c06127f94d6e25561f3ca23512b6ec38c63140
scoslo5512 <robert.queeney@gmail.com>
2018-07-21 18:15:25 -0700

    Starting the MDC integration

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,11 +1,14 @@
 import React from 'react';
+// import mdcInput from '@material/textfield/mdc-text-field';
 
 const Input = props => (
-	<div className="input-group">
-		<div className="input-group-prepend"> 
-			<span className="input-group-text">{props.title}</span>
+	<div class="mdc-text-field">
+		<div className="input-group">
+			<div className="input-group-prepend"> 
+				<span className="input-group-text">{props.title}</span>
+			</div>
+			<input className = "mdc-text-field__input" name={props.name} type={props.type} value={props.value} onChange={props.handleInput} className="form-control" aria-label="Input field" />
 		</div>
-		<input name={props.name} type={props.type} value={props.value} onChange={props.handleInput} className="form-control" aria-label="Input field" />
 	</div>
 );
 
